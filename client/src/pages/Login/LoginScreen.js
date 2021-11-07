2ae88a7107e61eeb0d9be1a22d30b894eef8b780
yigittopm <yigittopm@hotmail.com>
2021-11-07 17:57:02 +0300

    marquee added

--- client/src/pages/Login/LoginScreen.js
+++ client/src/pages/Login/LoginScreen.js
@@ -1,10 +1,33 @@
 import "../../style/LoginScreen.css"
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginScreen() {
+
+    const LoginSchema = Yup.object().shape({
+        email: Yup.string()
+          .email("Incorrect email.")
+          .min(
+            10, "Must be 10 or more"
+          )
+          .max(
+            100, "Must be 100 or less"
+          )
+          .required("Email is required"),
+        password: Yup.string()
+          .min(
+            6, "Must be 6 or more"
+          )
+          .max(
+            100, "Must be 100 or less"
+          )
+          .required("Password is required"),
+      });
+
     return (
         <div id="login-div" className="vh-100 justify-content-center align-items-center">
 
-            <h2 id="login-title">Login</h2>
+            <marquee className="row" id="login-title" behavior="alternate" direction="right" scrollAmount="10">Login</marquee>
 
             <form id="login-form" className="">
                 <div className="form-group">
--- client/src/pages/Register/RegisterScreen.js
+++ client/src/pages/Register/RegisterScreen.js
@@ -1,28 +1,100 @@
-//import { register } from "./redux/RegisterSlice"
-//import { useState, useEffect} from "react"
+import { register } from "./redux/RegisterSlice"
+import { Link, Redirect, useHistory } from "react-router-dom";
+import { useState, useEffect} from "react"
 import "../../style/RegisterScreen.css"
+import { useDispatch } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function RegisterScreen() {
 
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const initialValues = {
+        name:"",
+        email:"",
+        password:""
+    }
+
+    const RegisterSchema = Yup.object().shape({
+        name: Yup.string()
+          .min(
+            2, "Must be 2 or more"
+          )
+          .max(
+            100, "Must be 100 or less"
+          )
+          .required("Name is required"),
+        email: Yup.string()
+          .email("Incorrect email.")
+          .min(
+            10, "Must be 10 or more"
+          )
+          .max(
+            100, "Must be 100 or less"
+          )
+          .required("Email is required"),
+        password: Yup.string()
+          .min(
+            6, "Must be 6 or more"
+          )
+          .max(
+            100, "Must be 100 or less"
+          )
+          .required("Password is required"),
+      });
+
+     const { handleSubmit, handleChange, handleBlur, touched, values, errors } =
+    useFormik({
+      initialValues,
+      validationSchema: RegisterSchema,
+      onSubmit: async (values) => {
+        const { name, email, password } = values;
+        //const res = await dispatch(register(values));
+        console.log(values)
+      },
+    });
+
     return (
         <div id="register-div" className="vh-100 justify-content-center align-items-center">
+            <marquee className="row" id="register-title" behavior="alternate" direction="right" scrollAmount="5">Register</marquee>
 
-            <h2 id="register-title">Register</h2>
-
-            <form id="register-form" className="">
+            <form onSubmit={handleSubmit} id="register-form" className="">
                 <div className="form-group">
-                    <input className="form-control" placeholder="Name"/>
+                    <input 
+                        name="name" 
+                        type="text" 
+                        onChange={handleChange} 
+                        className="form-control" 
+                        value={values.name}
+                        placeholder="Name"/>
                 </div>
 
                 <div className="form-group">
-                    <input className="form-control" placeholder="Email"/>
+                    <input 
+                        name="email" 
+                        type="email" 
+                        onChange={handleChange} 
+                        className="form-control" 
+                        value={values.email}
+                        placeholder="Email"/>
                 </div>
 
                 <div className="form-group">
-                    <input className="form-control" placeholder="Password"/>
+                    <input 
+                        name="password" 
+                        type="password" 
+                        onChange={handleChange} 
+                        className="form-control" 
+                        value={values.password}
+                        placeholder="Password"/>
                 </div>
                 <div className="form-group">
-                    <button id="register-button" className="form-control btn">Register</button>
+                    <button 
+                        type="submit"
+                        id="register-button" 
+                        className="form-control btn">Register</button>
                 </div>
             </form>
         </div>
